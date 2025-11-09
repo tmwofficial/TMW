2551609192934beb618ec68047fabab502b18429
tmwofficial <marvinuso863@gmail.com>
2025-11-09 16:46:59 +0300

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,12 @@
+// Pre-made activation codes
+const validCodes = {
+  "TMW1111": { plan: "DAILY", days: 1 },
+  "TMW2222": { plan: "WEEKLY", days: 7 },
+  "TMW3333": { plan: "MONTHLY", days: 30 },
+  "TMW4444": { plan: "DAILY", days: 1 },
+  "TMW5555": { plan: "WEEKLY", days: 7 },
+  "TMW6666": { plan: "MONTHLY", days: 30 },
+  "TMW7777": { plan: "DAILY", days: 1 },
+  "TMW8888": { plan: "WEEKLY", days: 7 },
+  "TMW9999": { plan: "MONTHLY", days: 30 }
+};
