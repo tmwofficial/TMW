ab7ac02da46e6198c9120e46dc86737cd85808bf
tmwofficial <marvinuso863@gmail.com>
2025-12-03 11:04:06 +0300

    Add files via upload

---
+++ package.json.js
@@ -0,0 +1,28 @@
+{
+  "name": "tmw-website",
+  "version": "1.0.0",
+  "description": "TMW Talent Media Works - Africa's #1 Talent Platform",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": "16.x"
+  },
+  "keywords": [
+    "talent",
+    "uganda",
+    "media",
+    "musicians",
+    "djs",
+    "vjs",
+    "cinema"
+  ],
+  "author": "TMW Talent Media Works",
+  "license": "MIT"
+}
+
